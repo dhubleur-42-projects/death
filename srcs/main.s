5fc1f8fbf2261ac181bb88138c1e89fc45db8797
Jeremy Maia <jmaia@student.42.fr>
2025-03-23 16:19:03 +0100

    wip

--- srcs/main.s
+++ srcs/main.s
@@ -34,36 +34,36 @@ begin:
 ; void xor_cipher(char *data, int size, char *key, int key_size);
 ; xor_cipher(rdi data, rsi size, rdx key, rcx key_size);
 xor_cipher:
-	push rcx					; save key_size
-	push rdx					; save key
+	mov r9, 0					; i_key = 0;
+	mov r10, 0					; i_data = 0;
 
 	.loop:
-		cmp rsi, 0				; if (size == 0)
+		cmp r10, rsi 			; if (i_data == size)
 		je .end					; 	goto .end
 
-		mov al, [rdi]				; al = *data
-		mov bl, [rdx]				; bl = *key
-		xor al, bl				; al ^= bl
-		mov [rdi], al				; *data = al
+		mov r8, rdx				; cur_key_ptr = key
+		add r8, r9				; + i_key;
+		mov bl, [r8]			; bl = *cur_key_ptr
 
-		inc rdi					; data++
-		inc rdx					; key++
-		dec rsi					; size--
-		dec rcx					; key_size--
+		inc r9					; i_key++;
 
-		cmp rcx, 0				; if (key_size == 0)
-		je .key_reset				; 	goto .key_reset
+		mov r8, rdi				; cur_data_ptr = data
+		add r8, r10				; + i_data;
+
+		inc r10					; i_data++;
+
+		xor [r8], bl			; *cur_data_ptr ^= bl
+
+		xor r9, rcx				; if (i_key == key_size) //This if is for je, some lines below. Shuffling code
+		je .key_reset			; 	goto .key_reset
 
 		jmp .loop				; goto .loop
 
 	.key_reset:
-		mov rdx, [rsp]				; restore key
-		mov rcx, [rsp + 8]			; ...
+		mov r9, 0				; restore key
 		jmp .loop				; goto .loop
 
 	.end:
-		pop rdx					; reset stack
-		pop rcx					; ...
 		ret					; return
 ; TODO end transformable section
 
