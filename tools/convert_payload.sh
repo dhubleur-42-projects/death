3a0747ea095da37efa3c90dc288849a0a367d35c
Jeremy Maia <jmaia@student.42.fr>
2025-04-01 22:13:10 +0200

    Remove debug cmds from convert_payload.sh

--- tools/convert_payload.sh
+++ tools/convert_payload.sh
@@ -45,7 +45,6 @@ create_main_with_variations_buffer()
 		$NASM_CMD -I ./includes/ -felf64 -o "$WORK_FILE_NAME".o "$WORK_FILE_NAME".s
 
 		start_offset=$(nm "$WORK_FILE_NAME".o | grep "$BEGIN_TOKEN" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
-		true # TEMP
 		end_offset=$(nm "$WORK_FILE_NAME".o | grep "$END_TOKEN" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
 		size=$((end_offset-start_offset))
 
@@ -66,15 +65,10 @@ create_obfuscation_mains_to_merge()
 	$NASM_CMD -I ./includes/ -felf64 -o "$WORK_FOLDER"/main_with_only_anti_debugging.o "$WORK_FOLDER"/main_with_only_anti_debugging.s
 
 	start_offset=$(nm "$WORK_FOLDER"/main_with_only_anti_debugging.o | grep "can_run_infection.begin_anti_debugging" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
-		true # TEMP
-		true # TEMP
 	end_offset=$(nm "$WORK_FOLDER"/main_with_only_anti_debugging.o | grep "can_run_infection.end_anti_debugging" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
 	anti_debugging_size=$((end_offset-start_offset))
 
 	start_offset=$(nm "$WORK_FOLDER"/main_with_only_uncipher.o | grep "can_run_infection.begin_uncipher" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
-		true # TEMP
-		true # TEMP
-		true # TEMP
 	end_offset=$(nm "$WORK_FOLDER"/main_with_only_uncipher.o | grep "can_run_infection.end_uncipher" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
 	cipher_size=$((end_offset-start_offset))
 
@@ -107,10 +101,6 @@ create_final_main_with_obfuscation()
 	stop_address=$(nm "$WORK_FOLDER"/main_with_only_uncipher_balanced.elf | grep "can_run_infection.end_uncipher" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/obase=16;ibase=16;\U\1+1/' | bc | sed 's/^/0x/')
 
 	start_offset=$(objdump -F -d --start-address=$start_address --stop-address=$stop_address "$WORK_FOLDER"/main_with_only_uncipher_balanced.elf | grep -E "^[0-9a-z].*can_run_infection.*File Offset" | head -n1 | sed -E 's/.*File Offset: 0x([0-9a-z]*).*/ibase=16;\U\1+1/' | bc)
-		true # TEMP
-		true # TEMP
-		true # TEMP
-		true # TEMP
 	end_offset=$(objdump -F -d --start-address=$start_address --stop-address=$stop_address "$WORK_FOLDER"/main_with_only_uncipher_balanced.elf | grep -E "^[0-9a-z].*can_run_infection.*File Offset" | tail -n1 | sed -E 's/.*File Offset: 0x([0-9a-z]*).*/ibase=16;\U\1/' | bc)
 
 	# Xor between two code parts
@@ -144,11 +134,6 @@ update_final_main_with_variation()
 		$NASM_CMD -I ./includes/ -felf64 -o "$WORK_FILE_NAME".o "$WORK_FILE_NAME".s
 
 		start_offset=$(nm "$WORK_FILE_NAME".o | grep "$BEGIN_TOKEN" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
-		true # TEMP
-		true # TEMP
-		true # TEMP
-		true # TEMP
-		true # TEMP
 		end_offset=$(nm "$WORK_FILE_NAME".o | grep "$END_TOKEN" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/ibase=16;\U\1/' | bc)
 		size=$((end_offset-start_offset))
 		
@@ -166,12 +151,6 @@ update_final_main_with_variation()
 		stop_address=$(nm "$WORK_FILE_NAME".elf | grep "$END_TOKEN" | cut -d ' ' -f1 | sed -E 's/^0*([^0][0-9a-z]*)$/obase=16;ibase=16;\U\1+1/' | bc | sed 's/^/0x/')
 
 		start_offset=$(objdump -F -d --start-address=$start_address --stop-address=$stop_address "$WORK_FILE_NAME".elf | grep -E "^[0-9a-z].*$BEGIN_TOKEN.*File Offset" | head -n1 | sed -E 's/.*File Offset: 0x([0-9a-z]*).*/ibase=16;\U\1+1/' | bc)
-		true # TEMP
-		true # TEMP
-		true # TEMP
-		true # TEMP
-		true # TEMP
-		true # TEMP
 		end_offset=$(objdump -F -d --start-address=$start_address --stop-address=$stop_address "$WORK_FILE_NAME".elf | grep -E "^[0-9a-z].*$END_TOKEN.*File Offset" | tail -n1 | sed -E 's/.*File Offset: 0x([0-9a-z]*).*/ibase=16;\U\1/' | bc)
 		cat "$WORK_FILE_NAME".elf | head -c+$end_offset | tail -c+$start_offset > "$WORK_FILE_NAME".bin
 
